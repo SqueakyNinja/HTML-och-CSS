6d34051ac6ba5f0a3d39932a58225a0494f9052d
SqueakyNinja <rasmustagerud@gmail.com>
2020-10-30 11:41:54 +0100

    Navbar finished

--- Navbar/script.js
+++ Navbar/script.js
@@ -1,10 +1,14 @@
 function toggleHamburger() {
   document.querySelector("#button").classList.toggle("open");
-  document.querySelector(".navbarList").classList.toggle("show");
+  document.querySelector(".navbarListDiv").classList.toggle("show");
 }
 
 function toggleList() {
   document.querySelector("#arrowDown").classList.toggle("spin180");
-  document.querySelector(".dropdownContent").classList.toggle("toggleShow");
+  document.querySelector(".dropdownDiv").classList.toggle("toggleShow");
 }
 // document.querySelector(".dropdownContent").classList.toggle("show");
+function toggleList2() {
+  document.querySelector("#arrowDown2").classList.toggle("spin180");
+  document.querySelector(".dropdownContent2").classList.toggle("toggleShow");
+}
